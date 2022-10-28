83eb6a32aac5a3b5221da5ff1bdf4235d544a6f9
Osikoya17 <osikoyaolaoluwa2021@gmail.com>
2022-10-28 14:02:32 +0100

    logs showing on webpage

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,24 @@
 import { Invoice } from "./classes/invoice.js";
-const invOne = new Invoice("maria", "working", 230);
-const invTwo = new Invoice("yoshi", "walking", 500);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payment.js";
+// const invOne = new Invoice("maria","working",230) 
+// const invTwo = new Invoice("yoshi","walking",500) 
+// let docOne:HasFormatter
+// let docTwo:HasFormatter
+// docOne = new Invoice('yoshi','web work' ,250)
+// docTwo = new Payment('mario','plumbing work',200)
+// let docs:HasFormatter[] =[]
+// docs.push(docOne)
+// docs.push(docTwo)
+// console.log(docs);
+// let invoices:Invoice[]=[];
+// invoices.push(invOne)
+// invoices.push(invTwo)
+// invoices.forEach(inv =>{ 
+//     console.log(inv.client,inv.amount,inv.format());
+// })
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 const anchor = document.querySelector("a");
 console.log(anchor.href);
 // Type Casting
@@ -18,5 +30,14 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 from.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'Invoice') {
+        doc = new Invoice(tofrom.value, details.value, Number(amount.value));
+        console.log(doc);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, Number(amount.value));
+        console.log(doc);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
 import{Invoice} from "./classes/invoice.js"
-const invOne = new Invoice("maria","working",230) 
-const invTwo = new Invoice("yoshi","walking",500) 
+import { ListTemplate } from "./classes/ListTemplate.js"
+import {Payment} from "./classes/payment.js"
+import { HasFormatter } from "./interfaces/HasFormatter.js"
+// const invOne = new Invoice("maria","working",230) 
+// const invTwo = new Invoice("yoshi","walking",500) 
 
-let invoices:Invoice[]=[];
-invoices.push(invOne)
-invoices.push(invTwo)
+// let docOne:HasFormatter
+// let docTwo:HasFormatter
 
-invoices.forEach(inv =>{ 
-    console.log(inv.client,inv.amount,inv.format());
-})
+// docOne = new Invoice('yoshi','web work' ,250)
+// docTwo = new Payment('mario','plumbing work',200)
 
+// let docs:HasFormatter[] =[]
+// docs.push(docOne)
+// docs.push(docTwo)
+// console.log(docs);
+
+
+// let invoices:Invoice[]=[];
+// invoices.push(invOne)
+// invoices.push(invTwo)
+
+// invoices.forEach(inv =>{ 
+//     console.log(inv.client,inv.amount,inv.format());
+// })
+
+const ul = document.querySelector('ul')!
+const list = new ListTemplate(ul)
 const anchor = document.querySelector("a")!
 console.log(anchor.href);
 // Type Casting
@@ -21,13 +38,20 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement
 const details = document.querySelector('#details') as HTMLInputElement
 const amount= document.querySelector('#amount') as HTMLInputElement
 
+
+
 from.addEventListener("submit",(e:Event)=>{
     e.preventDefault()
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+    let doc:HasFormatter
+if (type.value === 'Invoice'){
+    doc = new Invoice(tofrom.value,details.value,Number(amount.value))
+    console.log(doc);
+}
+else{
+    doc  = new Payment(tofrom.value,details.value,Number(amount.value))
+    console.log(doc);
+    
+}
+list.render(doc,type.value,'end')
     
 })
